cb074b36236a9c4322e905f11c71f722ff7854ee
Rich Leland <rich@richleland.com>
2011-08-25 10:02:19 -0400

    skeleton commit

---
+++ example/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^collections/', include('collections.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+def get_readme():
+    """Return the README file contents. Supports text ,rst, and markdown"""
+    for name in ('README','README.rst','README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
+
+# Use the docstring of the __init__ file to be the description
+__import__('collections')
+DESC = " ".join(sys.modules['collections'].__doc__.splitlines()).strip()
+
+setup(
+    name = "collections",
+    version = sys.modules['collections'].get_version().replace(' ', '-'),
+    url = '',
+    license= '',
+    author = 'National Geographic',
+    author_email = 'appdev@ngs.org',
+    description = DESC,
+    long_description = get_readme(),
+    packages = find_packages(),
+    namespace_packages=[],
+    include_package_data = True,
+    install_requires = read_file('requirements.txt'),
+    classifiers = [], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+)
+
