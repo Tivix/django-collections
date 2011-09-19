51b6282f24215cef708c01bf51c05d10e08f3d9f
Matthew Farver <mfarver@tivix.com>
2011-09-19 16:20:18 -0600

    Allow COLLECTION_FILTER_FUNCTION to be a string

--- dynamic_collections/backends/base.py
+++ dynamic_collections/backends/base.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.utils.importlib import import_module
+from django.core.urlresolvers import get_mod_func
 
 class CollectionsSearchBackendBase(object):
     "An abstract CollectionsSearchBackend that enforces proper implementation"
@@ -11,6 +13,9 @@ class CollectionsSearchBackendBase(object):
         "Accepts a request and collection and returns a generic set of objects based on its db backend"
         if hasattr(settings, "COLLECTIONS_REQUEST_CLEANER"):
             request_cleaner = settings.COLLECTIONS_REQUEST_CLEANER
+            if not callable(request_cleaner):
+                mod_name, func_name = get_mod_func(request_cleaner)
+                request_cleaner = getattr(import_module(mod_name), func_name)
             return self.search(request_cleaner(request))
         else:
             raise Exception('COLLECTIONS_REQUEST_CLEANER setting not defined')
